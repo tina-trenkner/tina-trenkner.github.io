06d2b3c2e278f57192dea05e470376f810a9c44d
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2019-03-31 13:12:34 +1300

    fixes punct on Que Pasa

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 const greetings = [
-  "Kia Ora!", "Que pasa?", "Howdy!", "Oh Hai!"
+  "Kia Ora!", "¿Qué pasa?", "Howdy!", "Oh Hai!"
 ]
 
 function showGreeting() {
@@ -15,24 +15,24 @@ showGreeting();
 
 
 // Spotify footer
-
-const artistReq = new XMLHttpRequest();
-
-artistReq.addEventListener("load", showSpotify);
-getArtist();
-setInterval(getArtist, 30000);
-
-function getArtist() {
-  artistReq.open("GET", "https://api.spotify.com/v1/me/player/currently-playing");
-  artistReq.setRequestHeader('Authorization', 'Bearer BQAx8zIj4E45Qz0GxkfC2rNcNG3xpVhE2pOOyoEx1Eo9CtGsVtMR9t2eHzMIVZgnr1hH4eV2Fa2csfmGsnEQFXJc8LSz1euGNT2br1CaZO3VGT_ya6nocLXs02xJ9AZIudQ1kyjAKkcWZHQDndUwC8mAIQ' )
-  artistReq.send();
-}
-
-function showSpotify() {
-  const trackListing = JSON.parse(artistReq.responseText)
-    document.getElementById("spotify").innerHTML = "I'm currently listening to " + trackListing.item.album.artists[0].name + " on Spotify."
-}
+//
+// const artistReq = new XMLHttpRequest();
+//
+// artistReq.addEventListener("load", showSpotify);
+// getArtist();
+// setInterval(getArtist, 30000);
+//
+// function getArtist() {
+//   artistReq.open("GET", "https://api.spotify.com/v1/me/player/currently-playing");
+//   artistReq.setRequestHeader('Authorization', 'Bearer BQAx8zIj4E45Qz0GxkfC2rNcNG3xpVhE2pOOyoEx1Eo9CtGsVtMR9t2eHzMIVZgnr1hH4eV2Fa2csfmGsnEQFXJc8LSz1euGNT2br1CaZO3VGT_ya6nocLXs02xJ9AZIudQ1kyjAKkcWZHQDndUwC8mAIQ' )
+//   artistReq.send();
+// }
+//
+// function showSpotify() {
+//   const trackListing = JSON.parse(artistReq.responseText)
+//     document.getElementById("spotify").innerHTML = "I'm currently listening to " + trackListing.item.album.artists[0].name + " on Spotify."
+// }
 
 // for Spotify backend, use Webtask and Sendgrid for Spotify. Maybe Last.fm?
 
-const tinaFacts = ["I'm a Ravenclaw", "I'm an ISFJ", "I'm a damn Yankee", "I'm a Met fan", "I'm Latina", "I'm a Capricorn"]
+// const tinaFacts = ["I'm a Ravenclaw", "I'm an ISFJ", "I'm a damn Yankee", "I'm a Met fan", "I'm Latina", "I'm a Capricorn"]
